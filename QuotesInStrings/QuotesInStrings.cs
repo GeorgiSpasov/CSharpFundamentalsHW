63f36b18f5d6d9df8276336eb6d59e64cb973c93
Georgi Spasov <georgi.spasov@outlook.com>
2016-10-18 14:29:41 +0300

    Quotes In Strings

---
+++ QuotesInStrings/QuotesInStrings.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace QuotesInStrings
+{
+    class QuotesInStrings
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The \"use\" of quotations causes difficulties.");
+        }
+    }
+}
