9ce99e4aa56d1554fd1d6e7e554a4f6b9722c231
Georgi Spasov <georgi.spasov@outlook.com>
2016-10-18 14:46:25 +0300

    Print the ASCII Table

---
+++ PrintTheASCIITable/PrintTheASCIITable.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace PrintTheASCIITable
+{
+    class PrintTheASCIITable
+    {
+        static void Main(string[] args)
+        {
+            for (byte i = 33; i < 127; i++)
+            {
+                Console.Write((char)i);
+            }
+        }
+    }
+}
