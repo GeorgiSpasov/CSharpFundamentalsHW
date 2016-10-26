c499b91cdb76f1ff91756da62606d27f00d20334
Georgi Spasov <georgi.spasov@outlook.com>
2016-10-26 22:36:32 +0300

    Moon Gravity

---
+++ MoonGravity/MoonGravity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MoonGravity
+{
+    class MoonGravity
+    {
+        static void Main(string[] args)
+        {
+            double weight = double.Parse(Console.ReadLine());
+            Console.WriteLine("{0:f3}",weight*0.17);
+        }
+    }
+}
