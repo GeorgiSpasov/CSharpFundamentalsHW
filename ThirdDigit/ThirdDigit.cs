d98575392a596f6854a576270dbb69045f96c42c
Georgi Spasov <georgi.spasov@outlook.com>
2016-10-26 22:49:55 +0300

    Third Digit

---
+++ ThirdDigit/ThirdDigit.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ThirdDigit
+{
+    class ThirdDigit
+    {
+        static void Main(string[] args)
+        {
+            int digitEntered = int.Parse(Console.ReadLine());
+
+
+
+            if (digitEntered.ToString().Length >= 3 && digitEntered.ToString()[digitEntered.ToString().Length - 3] == '7')
+            {
+                Console.WriteLine("true");
+
+            }
+
+            else if (digitEntered.ToString().Length >= 3)
+            {
+
+                Console.WriteLine("false {0}", digitEntered.ToString()[digitEntered.ToString().Length - 3]);
+            }
+            else
+            {
+                Console.WriteLine("false 0");
+            }
+        }
+    }
+}
