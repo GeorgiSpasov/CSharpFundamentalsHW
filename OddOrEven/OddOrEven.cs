071c53d18dd98179e01aa2cccad1a08337236bcb
Georgi Spasov <georgi.spasov@outlook.com>
2016-10-26 22:31:32 +0300

    Odd or Even

---
+++ OddOrEven/OddOrEven.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace OddOrEven
+{
+    class OddOrEven
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+            Console.WriteLine((x%2==0)? "even {0}": "odd {0}", x);
+        }
+    }
+}
