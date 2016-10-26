056252aa1d04c74660481b8749b21f09b99cbe59
Georgi Spasov <georgi.spasov@outlook.com>
2016-10-27 01:57:44 +0300

    Prime Check

---
+++ PrimeCheck/PrimeCheck.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace PrimeCheck
+{
+    class PrimeCheck
+    {
+        static void Main(string[] args)
+        {
+            
+            int x = int.Parse(Console.ReadLine());
+            if ((x > 1) && (x < 100))
+            {
+                int divider = 2;
+                int maxDivider = (int)Math.Sqrt(x);
+                bool check = true;
+
+
+                while (check && (divider <= maxDivider))
+                {
+                    if ((x % divider) == 0)
+                    {
+                        check = false;
+                    }
+                    divider++;
+                }
+                
+                Console.WriteLine(check.ToString().ToLower());
+            }
+            else
+            {
+                Console.WriteLine("false");
+            }
+        }
+ 
+    }
+}
