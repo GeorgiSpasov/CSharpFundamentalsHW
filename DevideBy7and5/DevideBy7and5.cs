113ca4d31d09ab9a618e7fa086d0c47dc4657fa8
Georgi Spasov <georgi.spasov@outlook.com>
2016-10-26 22:42:50 +0300

    Devide by 5 and 7

---
+++ DevideBy7and5/DevideBy7and5.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DevideBy7and5
+{
+    class DevideBy7and5
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+            Console.WriteLine(((x % 5 == 0)&&(x % 7 == 0)) ? "true {0}" : "false {0}", x);
+        }
+    }
+}
