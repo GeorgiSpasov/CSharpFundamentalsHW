b2ae0f410c89d1db4fd56d8866755d4e0eb8fafe
Georgi Spasov <georgi.spasov@outlook.com>
2016-10-26 23:55:22 +0300

    Third Bit

---
+++ ThirdBit/ThirdBit.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ThirdBit
+{
+    class ThirdBit
+    {
+        static void Main(string[] args)
+        {
+            int p = 3;
+            int x = int.Parse(Console.ReadLine());
+            int mask = 1 << p;
+            int xAndMask = x & mask;
+            int bit = xAndMask >> p;
+            Console.WriteLine(bit);
+
+
+        }
+    }
+}
