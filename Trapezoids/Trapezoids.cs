90dab1548bda27387ba561e040091ea4fbe7a89c
Georgi Spasov <georgi.spasov@outlook.com>
2016-10-26 23:50:56 +0300

    Trapezoids

---
+++ Trapezoids/Trapezoids.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Trapezoids
+{
+    class Trapezoids
+    {
+        static void Main(string[] args)
+        {
+            double a = double.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+            double h = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("{0:F7}", (a+b)/2*h);
+
+        }
+    }
+}
