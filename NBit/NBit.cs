793c66a569e4b971fffba1ec1e199ac1dc0a052d
Georgi Spasov <georgi.spasov@outlook.com>
2016-10-27 00:21:12 +0300

    N-th Bit

---
+++ NBit/NBit.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace NBit
+{
+    class NBit
+    {
+        static void Main(string[] args)
+        {
+            long p = long.Parse(Console.ReadLine());
+            int n = int.Parse(Console.ReadLine()); 
+            Console.WriteLine((p>>n)&1);  
+
+        }
+    }
+}
