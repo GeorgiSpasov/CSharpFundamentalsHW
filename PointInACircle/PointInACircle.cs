0a4aec8a45925c4d53cc266b162b8837042e1359
Georgi Spasov <georgi.spasov@outlook.com>
2016-10-26 22:58:30 +0300

    Point in a Circle

---
+++ PointInACircle/PointInACircle.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace PointInACircle
+{
+    class PointInACircle
+    {
+        static void Main(string[] args)
+        {
+            double dotX = double.Parse(Console.ReadLine());
+            double dotY = double.Parse(Console.ReadLine());
+            double circleCenter_x = 0;
+            double circleCenter_y = 0;
+
+            double dx = Math.Abs(dotX - circleCenter_x);
+            double dy = Math.Abs(dotY - circleCenter_y);
+            double Radius = 2;
+
+
+            if (Radius >= Math.Sqrt(dx * dx + dy * dy))
+            {
+                Console.Write("yes ");
+            }
+            else
+            {
+                Console.Write("no ");
+            }
+            Console.WriteLine("{0:F2}", Math.Round(Math.Sqrt(dx * dx + dy * dy), 2));
+
+        }
+    }
+}
