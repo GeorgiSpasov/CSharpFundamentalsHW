9df6229a9ada04bd4c17ef41d6643523c59041e3
Georgi Spasov <georgi.spasov@outlook.com>
2016-10-27 03:18:40 +0300

    Modify Bit

---
+++ ModifyBit/ModifyBit.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ModifyBit
+{
+    class ModifyBit
+    {
+        static void Main(string[] args)
+        {
+            long number = long.Parse(Console.ReadLine());
+            byte position = byte.Parse(Console.ReadLine());
+            byte value = byte.Parse(Console.ReadLine());
+
+            number ^= (-value ^ number) & (1 << position);
+
+            Console.WriteLine(number);
+
+            
+        }
+    }
+}
