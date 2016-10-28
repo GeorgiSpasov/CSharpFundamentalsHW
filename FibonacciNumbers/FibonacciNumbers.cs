78e91e2988f56defa059317590bc9199493cc7a4
Georgi Spasov <georgi.spasov@outlook.com>
2016-10-28 23:43:21 +0300

    Fibonacci Numbers

---
+++ FibonacciNumbers/FibonacciNumbers.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace FibonacciNumbers
+{
+    class FibonacciNumbers
+    {
+        static void Main(string[] args)
+        {
+            byte n = byte.Parse(Console.ReadLine());
+            
+            int a = -1;
+            int b = 1;
+            int c = 0;
+
+            for (int i = 0; i < (n-1); i++)
+            {
+                c = a + b;
+
+                Console.Write("{0}, ", c);
+                a = b;
+                b = c;
+            }
+            Console.WriteLine(a+b);
+        }
+    }
+}
