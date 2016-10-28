146d4aacf3d5269c35625b9a3453c093b5329e03
Georgi Spasov <georgi.spasov@outlook.com>
2016-10-28 22:28:55 +0300

    Sum of Five Numbers

---
+++ SumOfFiveNumbers/SumOfFiveNumbers.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SumOfFiveNumbers
+{
+    class SumOfFiveNumbers
+    {
+        static void Main(string[] args)
+        {
+            short a = short.Parse(Console.ReadLine());
+            short b = short.Parse(Console.ReadLine());
+            short c = short.Parse(Console.ReadLine());
+            short d = short.Parse(Console.ReadLine());
+            short e = short.Parse(Console.ReadLine());
+            Console.WriteLine(a + b + c + d + e);
+        }
+    }
+}
