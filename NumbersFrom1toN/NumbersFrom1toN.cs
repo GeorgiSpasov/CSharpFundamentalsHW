6f93e0b28233fabac40f55517122edd4f09a5dba
Georgi Spasov <georgi.spasov@outlook.com>
2016-10-28 22:32:17 +0300

    Numbers From 1 to N

---
+++ NumbersFrom1toN/NumbersFrom1toN.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace NumbersFrom1toN
+{
+    class NumbersFrom1toN
+    {
+        static void Main(string[] args)
+        {
+            short n = short.Parse(Console.ReadLine());
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+        }
+    }
+}
