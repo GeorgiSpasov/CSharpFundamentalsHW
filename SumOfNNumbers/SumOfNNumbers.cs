1d0b27d4ab4f961685fc6d613a6fd5e16352ed9a
Georgi Spasov <georgi.spasov@outlook.com>
2016-10-28 23:12:51 +0300

    Sum of N Numbers

---
+++ SumOfNNumbers/SumOfNNumbers.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace SumOfNNumbers
+{
+    class SumOfNNumbers
+    {
+        static void Main(string[] args)
+        {
+            byte n = byte.Parse(Console.ReadLine());
+            double sum = 0;
+            
+            for (int i = 0; i < n; i++)
+            {
+                sum += double.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
