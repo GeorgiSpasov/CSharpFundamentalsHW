b5d2764270452772845165141989ff5f5e04e38f
Georgi Spasov <georgi.spasov@outlook.com>
2016-10-28 22:05:33 +0300

    Circle

---
+++ Circle/Circle.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Circle
+{
+    class Circle
+    {
+        static void Main(string[] args)
+        {
+            double r = double.Parse(Console.ReadLine());
+            double perimeter = 2 * Math.PI * r;
+            double area = Math.PI * r * r;
+
+            Console.WriteLine("{0:F2} {1:F2}", perimeter, area);
+        }
+    }
+}
