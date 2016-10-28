21562f9b9285132595fe7396a3519d9fb88751c0
Georgi Spasov <georgi.spasov@outlook.com>
2016-10-28 22:22:02 +0300

    Quadratic Equation

---
+++ QuadraticEquation/QuadraticEquation.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace QuadraticEquation
+{
+    class QuadraticEquation
+    {
+        static void Main(string[] args)
+        {
+            double a = double.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+            double c = double.Parse(Console.ReadLine());
+            double discriminant = b*b - 4*a*c;
+
+            if (discriminant > 0)
+            {
+                Console.WriteLine("{0:f2}",((-b) - Math.Sqrt(discriminant)) / (2 * a));
+                Console.WriteLine("{0:f2}",((-b) + Math.Sqrt(discriminant)) / (2 * a));
+            }
+            else if (discriminant == 0)
+            {
+                Console.WriteLine("{0:f2}", ((-b) - Math.Sqrt(discriminant)) / (2 * a));
+            }
+            else
+            {
+                Console.WriteLine("no real roots");
+            }
+
+        }
+    }
+}
