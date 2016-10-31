a1657c6929f8d8f29939fd9b12e257b3b7343205
Georgi Spasov <georgi.spasov@outlook.com>
2016-10-31 10:57:54 +0200

    Biggest of 3

---
+++ BiggestOf3/BiggestOf3.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace BiggestOf3
+{
+    class BiggestOf3
+    {
+        static void Main(string[] args)
+        {
+            double num1 = double.Parse(Console.ReadLine());
+            double num2 = double.Parse(Console.ReadLine());
+            double num3 = double.Parse(Console.ReadLine());
+
+            if (num1>num2 && num1>num3)
+            {
+                Console.WriteLine(num1);
+            }
+            else if (num1<num2 && num2>num3)
+            {
+                Console.WriteLine(num2);
+            }
+            else
+            {
+                Console.WriteLine(num3);
+            }
+        }
+    }
+}
