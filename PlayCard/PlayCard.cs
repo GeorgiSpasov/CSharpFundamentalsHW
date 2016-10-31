2124aaa7322d2fba4a41466448e4d55680c2ef99
Georgi Spasov <georgi.spasov@outlook.com>
2016-10-31 10:19:09 +0200

    Play Card

---
+++ PlayCard/PlayCard.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace PlayCard
+{
+    class PlayCard
+    {
+        static void Main(string[] args)
+        {
+            string inputCard = Console.ReadLine();
+
+            string[] cardFaces = { "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A" };
+
+            if (cardFaces.Contains(inputCard))
+            {
+                Console.WriteLine("yes {0}", inputCard);
+            }
+            else
+            {
+                Console.WriteLine("no {0}", inputCard);
+            }
+        }
+    }
+}
