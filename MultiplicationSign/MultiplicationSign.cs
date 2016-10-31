a3380276a14930165adb839bbbf1460e70d3e72c
Georgi Spasov <georgi.spasov@outlook.com>
2016-10-31 10:51:05 +0200

    Multiplication Sign

---
+++ MultiplicationSign/MultiplicationSign.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace MultiplicationSign
+{
+    class MultiplicationSign
+    {
+        static void Main(string[] args)
+        {
+            double num1 = double.Parse(Console.ReadLine());
+            double num2 = double.Parse(Console.ReadLine());
+            double num3 = double.Parse(Console.ReadLine());
+
+            if ((num1 == 0) || (num2 == 0) || (num3 == 0))
+            {
+                Console.WriteLine(0);
+            }
+            else if ((num1 < 0 && num2 > 0 && num3 > 0) || 
+                     (num1 > 0 && num2 < 0 && num3 > 0) || 
+                     (num1 > 0 && num2 > 0 && num3 < 0) || 
+                     (num1 < 0 && num2 < 0 && num3 < 0))
+            {
+                Console.WriteLine("-");
+            }
+            else
+            {
+                Console.WriteLine("+");
+            }
+        }
+    }
+}
