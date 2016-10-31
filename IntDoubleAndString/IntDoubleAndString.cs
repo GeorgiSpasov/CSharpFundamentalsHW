77d78db4c0b7e39350f5f0cc5dc9cd286502a190
Georgi Spasov <georgi.spasov@outlook.com>
2016-10-31 12:21:16 +0200

    Int, Double and String

---
+++ IntDoubleAndString/IntDoubleAndString.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace IntDoubleAndString
+{
+    class IntDoubleAndString
+    {
+        static void Main(string[] args)
+        {
+            string type = Console.ReadLine();
+            string variable = Console.ReadLine();
+
+            switch (type)
+            {
+                case "integer":
+                    Console.WriteLine(int.Parse(variable) + 1);
+                    break;
+
+                case "real":
+                    Console.WriteLine("{0:f2}", double.Parse(variable) + 1);
+                    break;
+
+                case "text":
+                    Console.WriteLine(variable + "*");
+                    break;
+            }
+        }
+    }
+}
