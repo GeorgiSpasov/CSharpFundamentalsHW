ff25703689ad87d85a97cfefaf17d670f85073bc
Georgi Spasov <georgi.spasov@outlook.com>
2016-10-31 10:10:39 +0200

    Exchange If Greater

---
+++ ExchangeIfGreater/ExchangeIfGreater.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ExchangeIfGreater
+{
+    class ExchangeIfGreater
+    {
+        static void Main(string[] args)
+        {
+            double numberOne = double.Parse(Console.ReadLine());
+            double numberTwo = double.Parse(Console.ReadLine());
+
+            if (numberOne < numberTwo)
+            {
+                Console.WriteLine("{0} {1}", numberOne, numberTwo);
+            }
+            else
+            {
+                Console.WriteLine("{0} {1}", numberTwo, numberOne);
+            }
+        }
+    }
+}
