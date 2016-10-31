9f3960ac7ac2702e015a06d65d298466978f93e5
Georgi Spasov <georgi.spasov@outlook.com>
2016-10-31 11:59:02 +0200

    Digit as Word

---
+++ DigitAsWord/DigitAsWord.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace DigitAsWord
+{
+    class DigitAsWord
+    {
+        static void Main(string[] args)
+        {
+            string[] words = { "zero", "one", "two", "three", "four",
+                               "five", "six", "seven", "eight", "nine" };
+            int digit;
+
+            if ((int.TryParse(Console.ReadLine(), out digit)) && digit >= 0 && digit <= 9)
+            {
+                Console.WriteLine(words[digit]);
+            }
+            else
+            {
+                Console.WriteLine("not a digit");
+            }
+        }
+    }
+}
