6bdf411260b3bcfd4ee606aa13e921d224ea83cc
Georgi Spasov <georgi.spasov@outlook.com>
2016-10-31 23:04:36 +0200

    Numbers From 1 to N v2

---
+++ NumbersFrom1toNv2/NumbersFrom1toNv2.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace NumbersFrom1toNv2
+{
+    class NumbersFrom1toNv2
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= n; i++)
+            {
+                Console.Write("{0} ",i);
+            }
+
+        }
+    }
+}
