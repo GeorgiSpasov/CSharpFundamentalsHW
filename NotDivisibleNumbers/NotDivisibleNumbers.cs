0af3c991ce69483a8a918c78d2fe833dfb724474
Georgi Spasov <georgi.spasov@outlook.com>
2016-10-31 23:13:27 +0200

    Not Divisible Numbers

---
+++ NotDivisibleNumbers/NotDivisibleNumbers.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace NotDivisibleNumbers
+{
+    class NotDivisibleNumbers
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i <= n; i++)
+            {
+                if (i % 3 == 0 || i% 7 ==0)
+                {
+                    continue;
+                }
+                Console.Write("{0} ", i);
+
+                ////first version
+                //int inputNumber = int.Parse(Console.ReadLine());
+
+                //for (int i = 1; i <= inputNumber; i++)
+                //{
+
+                //    if (i % 3 != 0 && i % 7 != 0)
+                //    {
+                //        Console.Write("{0} ", i);
+                //    }
+
+                //}
+            }
+        }
+    }
+}
