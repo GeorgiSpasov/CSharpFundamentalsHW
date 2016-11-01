35de75d1fc8959ce6ef952008a52bfd5f223c69d
Georgi Spasov <georgi.spasov@outlook.com>
2016-11-01 15:04:28 +0200

    Calculate 3!

---
+++ Calculate3/Calculate3.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Numerics;
+
+namespace Calculate3
+{
+    class Calculate3
+    {
+        static void Main(string[] args)
+        {
+            double n = int.Parse(Console.ReadLine());
+            double k = int.Parse(Console.ReadLine());
+            double nMinusK = n - k;
+            BigInteger factnMk = 1;
+            BigInteger factN = 1;
+            BigInteger factK = 1;
+
+            for (int i = 1; i <= n; i++)
+            {
+                if (i <= k && i <= nMinusK)
+                {
+                    factN *= i;
+                    factK *= i;
+                    factnMk *= i;
+                }
+                else if (i <= k && i > nMinusK)
+                {
+                    factN *= i;
+                    factK *= i;
+                }
+                else if (i > k && i <= nMinusK)
+                {
+                    factN *= i;
+                    factnMk *= i;
+                }
+                else
+                {
+                    factN *= i;
+                }
+            }
+            Console.WriteLine(factN / (factK * factnMk));
+        }
+    }
+}
