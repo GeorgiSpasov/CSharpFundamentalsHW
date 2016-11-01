71fcbf27fc23a1202fff28782d352e39a01b95c0
Georgi Spasov <georgi.spasov@outlook.com>
2016-11-01 14:40:27 +0200

    Calculate

---
+++ Calculate/Calculate.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Calculate
+{
+    class Calculate
+    {
+        static void Main(string[] args)
+        {
+            double N = double.Parse(Console.ReadLine());
+            double x = double.Parse(Console.ReadLine());
+
+            double sum = 1 + 1 / x;
+
+            long fact = 1;
+            for (int i = 2; i <= N; i++)
+            {
+                fact *= i;
+                sum += fact / Math.Pow(x, i);
+            }
+            Console.WriteLine("{0:F5}", sum);
+        }
+    }
+}
