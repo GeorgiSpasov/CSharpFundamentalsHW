c52be76c925b18613d85f17baa746ea61be3b01c
Georgi Spasov <georgi.spasov@outlook.com>
2016-11-01 14:53:46 +0200

    Calculate Again!

---
+++ CalculateAgain/CalculateAgain.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Numerics;
+
+namespace CalculateAgain
+{
+    class CalculateAgain
+    {
+        static void Main(string[] args)
+        {
+            BigInteger n = BigInteger.Parse(Console.ReadLine());
+            BigInteger k = BigInteger.Parse(Console.ReadLine());
+
+            BigInteger fact = 1;
+
+            for (BigInteger i = n; i > k; i--)
+            {
+                fact = fact * i;
+            }
+            Console.WriteLine(fact);
+        }
+    }
+}
