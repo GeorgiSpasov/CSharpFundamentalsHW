2b82037102a2ece302c4cb31cd8e3a2e870d344e
Georgi Spasov <georgi.spasov@outlook.com>
2016-11-02 16:41:52 +0200

    Matrix of Numbers

---
+++ MatrixOfNumbers/MatrixOfNumbers.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MatrixOfNumbers
+{
+    class MatrixOfNumbers
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            
+            int counter = n;
+
+            for (int i = 1; i <= n; i++)
+            {
+                counter++;
+
+                for (int j = i; j < counter; j++)
+                {
+                    Console.Write("{0} ",j);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
