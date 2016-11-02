b8a3c4a9934a865a5972ecdefa0555ff4cc760df
Georgi Spasov <georgi.spasov@outlook.com>
2016-11-02 23:12:16 +0200

    Decimal to Hex

---
+++ DecimalToHex/DecimalToHex.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace DecimalToHex
+{
+    class DecimalToHex
+    {
+        static void Main(string[] args)
+        {
+            long decimalNumber = long.Parse(Console.ReadLine());
+            string numbers = "0123456789ABCDEF";
+            string hexNumber = null;
+
+            while (decimalNumber > 0)
+            {
+                hexNumber = numbers[Convert.ToInt32(decimalNumber % 16)] + hexNumber;
+                decimalNumber /= 16;
+            }
+
+            Console.WriteLine(hexNumber);
+        }
+    }
+}
