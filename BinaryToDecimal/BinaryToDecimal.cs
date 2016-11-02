e6088970f92a042bb09ad52b43161d32f1307753
Georgi Spasov <georgi.spasov@outlook.com>
2016-11-02 22:21:58 +0200

    Binary to Decimal

--- BinaryToDecimal/BinaryToDecimal.cs
+++ BinaryToDecimal/BinaryToDecimal.cs
@@ -6,27 +6,16 @@ namespace BinaryToDecimal
     {
         static void Main(string[] args)
         {
-            int decimalNumber = 0;
-            
+            long decimalNumber = 0;
 
             string binary = Console.ReadLine();
 
-            for (int index = binary.Length-1, power = 0; index > 0; index--, power++)
+            for (int index = binary.Length-1, power = 0; index >= 0; index--, power++)
             {
-
-                //Console.WriteLine(binary[index]);
-                //Console.WriteLine(power);
-                //Console.WriteLine(Math.Pow(2, power));
                 if ((binary[index] - '0') != 0)
                 {
                     decimalNumber = decimalNumber + (binary[index] - '0') * (int)Math.Pow(2, power);
                 }
-                else
-                {
-                    continue;
-                }
-
-
             }
 
             Console.WriteLine(decimalNumber);
