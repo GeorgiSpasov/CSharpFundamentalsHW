a44d7a32020bc5b65e65faafd3ea4443b21bc7bb
Georgi Spasov <georgi.spasov@outlook.com>
2016-11-02 20:03:58 +0200

    Working on Binary to Decimal

---
+++ BinaryToDecimal/BinaryToDecimal.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace BinaryToDecimal
+{
+    class BinaryToDecimal
+    {
+        static void Main(string[] args)
+        {
+            int decimalNumber = 0;
+            
+
+            string binary = Console.ReadLine();
+
+            for (int index = binary.Length-1, power = 0; index > 0; index--, power++)
+            {
+
+                //Console.WriteLine(binary[index]);
+                //Console.WriteLine(power);
+                //Console.WriteLine(Math.Pow(2, power));
+                if ((binary[index] - '0') != 0)
+                {
+                    decimalNumber = decimalNumber + (binary[index] - '0') * (int)Math.Pow(2, power);
+                }
+                else
+                {
+                    continue;
+                }
+
+
+            }
+
+            Console.WriteLine(decimalNumber);
+        }
+    }
+}
