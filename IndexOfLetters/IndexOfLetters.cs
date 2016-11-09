a7fc9435b0693e9d077dac5a7aa451f1c0bbfdba
Georgi Spasov <georgi.spasov@outlook.com>
2016-11-10 00:33:49 +0200

    Index of letters

---
+++ IndexOfLetters/IndexOfLetters.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace IndexOfLetters
+{
+    class IndexOfLetters
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+            char start = 'a';
+            for (int i = 0; i < word.Length; i++)
+            {
+                Console.WriteLine((int)word[i] - (int)start);
+            }
+        }
+    }
+}
