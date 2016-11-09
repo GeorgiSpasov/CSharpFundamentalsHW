a66c9c7176448042335ff1143fa84433da7f8af4
Georgi Spasov <georgi.spasov@outlook.com>
2016-11-10 00:32:34 +0200

    Binary search

---
+++ BinarySearch/BinarySearch.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace BinarySearch
+{
+    class BinarySearch
+    {
+        static void Main(string[] args)
+        {
+            int size = int.Parse(Console.ReadLine());
+            int[] array = new int[size];
+            for (int i = 0; i < size; i++)
+            {
+                array[i] = int.Parse(Console.ReadLine());
+            }
+            Array.Sort(array);
+            int x = int.Parse(Console.ReadLine());
+
+            int index = Array.BinarySearch(array, x);
+            if (index < 0)
+            {
+                Console.WriteLine("-1");
+            }
+            else
+            {
+                Console.WriteLine(index);
+            }
+        }
+    }
+}
