54aab2d268daf34cd1364383851d196e60d10b6a
Georgi Spasov <georgi.spasov@outlook.com>
2016-11-09 22:16:38 +0200

    Maximal increasing sequence

---
+++ MaximalIncreasingSequence/MaximalIncreasingSequence.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace MaximalIncreasingSequence
+{
+    class MaximalIncreasingSequence
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] intArray = new int[n];
+
+            int counter = 1;
+            int current = 1;
+            int longest = 0;
+
+            for (int i = 0; i < intArray.Length; i++)
+            {
+                intArray[i] = int.Parse(Console.ReadLine());
+            }
+
+            //counting
+            for (int k = 1; k < intArray.Length; k++)
+            {
+                if (intArray[k] <= intArray[k - 1])
+                {
+                    counter = 1;
+                }
+                else if (intArray[k] > intArray[k - 1])
+                {
+                    counter++;
+                    current = counter;
+                }
+                if (longest < current)
+                {
+                    longest = current;
+                }
+            }
+            
+            //the result
+            Console.WriteLine(longest);
+        }
+    }
+}
