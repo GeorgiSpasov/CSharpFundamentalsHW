5a312be159992458f50b0387528569430120bebf
Georgi Spasov <georgi.spasov@outlook.com>
2016-11-10 00:29:40 +0200

    Maximal sum

---
+++ MaximalSum/MaximalSum.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace MaximalSum
+{
+    class MaximalSum
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[] nums = new int[n];
+
+            for (int i = 0; i < n; i++)
+                nums[i] = int.Parse(Console.ReadLine());
+
+            int max = nums[0];
+            int currmax = nums[0];
+
+            for (int i = 1; i < n; i++)
+            {
+                currmax = Math.Max(nums[i], currmax + nums[i]);
+                if (currmax > max)
+                    max = currmax;
+            }
+            Console.WriteLine(max);
+        }
+    }
+}
