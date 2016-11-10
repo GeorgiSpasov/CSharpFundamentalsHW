1c8de4f2886007c61122af58ef57c7eaa9878428
Georgi Spasov <georgi.spasov@outlook.com>
2016-11-10 17:05:46 +0200

    Mixing numbers

---
+++ MixingNumbers/MixingNumbers.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace MixingNumbers
+{
+    class MixingNumbers
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] intArray = new int[n];
+
+            for (int i = 0; i < intArray.Length; i++)
+            {
+                intArray[i] = int.Parse(Console.ReadLine());
+            }
+
+            //Mixing
+            for (int j = 1; j < intArray.Length; j++)
+            {
+                Console.Write("{0} ", (intArray[j - 1] % 10) * (intArray[j] / 10));
+            }
+            Console.WriteLine();
+
+            //Subtracting
+            for (int k = 1; k < intArray.Length; k++)
+            {
+                Console.Write("{0} ", Math.Abs(intArray[k - 1] - intArray[k]));
+            }
+        }
+    }
+}
--- MixingNumbers/Properties/AssemblyInfo.cs
+++ MixingNumbers/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("BirdsAndFeathers")]
+[assembly: AssemblyTitle("MixingNumbers")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("BirdsAndFeathers")]
+[assembly: AssemblyProduct("MixingNumbers")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("24755530-b0ff-4803-b26e-a8612d6f5f5e")]
+[assembly: Guid("f04a6724-6dc9-44b5-8960-171d4e8a7c43")]
 
 // Version information for an assembly consists of the following four values:
 //
