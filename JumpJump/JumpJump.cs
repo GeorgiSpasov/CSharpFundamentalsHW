66544dc326ba7a4a16ea96b2f146a5dc03128cd3
Georgi Spasov <georgi.spasov@outlook.com>
2016-11-10 17:03:17 +0200

    Jump Jump

---
+++ JumpJump/JumpJump.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace JumpJump
+{
+    class JumpJump
+    {
+        static void Main(string[] args)
+        {
+            string instructions = Console.ReadLine();
+            int i = 0;
+            
+            do
+            {
+                if ((i >= 0 && i < instructions.Length) && instructions[i] == '0')
+                {
+                    Console.WriteLine("Too drunk to go on after {0}!", i); //Too drunk to go on after POSITION!
+                    break;
+                }
+                else if ((i >= 0 && i < instructions.Length) && instructions[i] == '^')
+                {
+                    Console.WriteLine("Jump, Jump, DJ Tomekk kommt at {0}!", i); //Jump, Jump, DJ Tomekk kommt at POSITION!
+                    break;
+                }
+                if ((i >= 0 && i < instructions.Length) && (instructions[i] - 48) % 2 == 0)
+                {
+                    i = i + (instructions[i] - 48);
+                }
+                else if ((i >= 0 && i < instructions.Length) && (instructions[i] - 48) % 2 != 0)
+                {
+                    i = i - (instructions[i] - 48);
+                }
+                else if (i < 0 || i >= instructions.Length)
+                {
+                    Console.WriteLine("Fell off the dancefloor at {0}!", i); //Fell off the dancefloor at POSITION!
+                    break;
+                }
+            } while (i >= int.MinValue && i < int.MaxValue);            
+        }
+    }
+}
